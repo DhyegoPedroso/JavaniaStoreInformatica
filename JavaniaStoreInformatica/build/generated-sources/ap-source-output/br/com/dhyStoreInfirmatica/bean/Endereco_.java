c2b51ed68dcebc86708892d582c09fd084149375
Dhyego Pedroso <dhyego.pedroso@outlook.com>
2019-10-26 01:23:24 -0300

    Tela Cliente salvando Os dados do Cliente.
    OBS: Ao add novo motorista ou remover o motorista sistema nao esta atualizando a tabela de caminhão. Mas ele realiza o procedimento corretamente. Verificar o motivo desse BUG

--- JavaniaStoreInformatica/build/generated-sources/ap-source-output/br/com/dhyStoreInfirmatica/bean/Endereco_.java
+++ JavaniaStoreInformatica/build/generated-sources/ap-source-output/br/com/dhyStoreInfirmatica/bean/Endereco_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-07-03T18:33:01")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-10-26T00:19:29")
 @StaticMetamodel(Endereco.class)
 public class Endereco_ { 
 
